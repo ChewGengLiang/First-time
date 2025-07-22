d3d7bb94fa20ec65c722d309ed2cf41598b9c9ed
MegapaoowChew <25041807@imail.sunway.edu.my>
2025-07-22 10:52:25 +0000

    Hey

---
+++ Part2/ex11.py
@@ -0,0 +1,10 @@
+result=0
+def add_two(num) :
+    return(num + 2)
+
+print(add_two(4))
+print(add_two(8))
+
+# Or store the result in a variable and print it
+#result = add_two(8)
+#print(result)
---
+++ Part2/ex11b.py
@@ -0,0 +1,7 @@
+def calculate(num1, num2, operator) :
+    if operator == "+" :
+        return(num1+num2)
+    elif operator == "-" :
+        return(num1-num2)
+    elif operator == "*" :
+        return 
